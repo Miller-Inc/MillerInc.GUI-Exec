3d7d5e6db13868e95d909616a8c7fa8b17de935b
JWMiller006 <jwmiller006@outlook.com>
2024-10-30 21:09:21 -0400

    initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include "lib/MillerInc.GUI-Library/include/GUI.h"
+
+int main()
+{
+    Library::hello();
+
+    Window window(800, 600, 0, 0, new std::string("Hello World!"));
+
+    window.show();
+    return 0;
+}
