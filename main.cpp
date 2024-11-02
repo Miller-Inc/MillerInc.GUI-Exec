dc6714e19c073c9afe87adb7f1ce2612bce96f95
J Miller <jwmiller006@outlook.com>
2024-11-01 20:02:49 -0400

    Support Linux

--- main.cpp
+++ main.cpp
@@ -1,12 +1,40 @@
-﻿#include <iostream>
-#include "lib/MillerInc.GUI-Library/include/GUI.h"
+﻿#include <chrono>
+#include <iostream>
+#include <thread>
+
+#include "libs/MillerInc.GUI-Library/include/GUI.h"
+#include "libs/MillerInc.GUI-Library/include/Window/Linux/LinuxWindow.h"
 
 int main()
 {
     Library::hello();
 
-    Window window(800, 600, 0, 0, new std::string("Hello World!"));
+    CustomWindow* window = nullptr;
 
-    window.show();
+#if defined WINDOWS
+    window = new WindowsWindow(800, 600, 100, 100, new std::string("Windows Window"));
+#elif defined LINUX || defined DEBUG
+    window = new LinuxWindow(800, 600, 100, 100, new std::string("Linux Window"));
+#endif
+
+#if defined LINUX || defined DEBUG
+    if (auto* linux_window = dynamic_cast<LinuxWindow*>(window)) {
+        linux_window->setWindowColor(0x00000F); // Set window color to black
+        linux_window->setButtonColor(0x0000FF); // Set button color to black
+
+        const std::string& lbl1 = "Button";
+        const std::string& lbl2 = "Button";
+
+        linux_window->addButton(new Button(1, 100, 10, 100, 100, lbl1));
+        linux_window->addLabel(new Label(2, 10, 10, 10, 10, lbl2));
+
+        linux_window->show();
+
+        std::cout << "Window updated\n";
+        linux_window->update();
+        std::cout << "Window updated\n";
+    }
+#endif
+
     return 0;
 }
